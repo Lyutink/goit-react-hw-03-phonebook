f1f8a6394a90abe8b64711034ace1cf7dd9ea99e
Lyutink <krivenko.lyuda@gmail.com>
2022-01-21 12:37:58 +0300

    src

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+
+import ContactForm from "./Components/ContactForm/ContactForm";
+import ContactList from "./Components/ContactList/ContactList";
+import Filter from "./Components/Filter/Filter";
+
+import { TitleForm } from "./Components/ContactForm/ContactForm.styled";
+import { TitleContacts } from "./Components/Contact/Contact.styled";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+    name: "",
+    number: "",
+  };
+
+  checkContact = (name) => {
+    const { contacts } = this.state;
+    const normilizedName = name.toLowerCase();
+    return contacts.find(
+      (contact) => normilizedName === contact.name.toLowerCase()
+    );
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const contact = { id: nanoid(), name, number };
+
+    if (this.checkContact(name)) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+    }
+  };
+
+  getContactList = () => {
+    const { contacts, filter } = this.state;
+    const normilizedFilter = filter.toLocaleLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLocaleLowerCase().includes(normilizedFilter)
+    );
+  };
+
+  changeFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  onDeleteContact = (idContact) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== idContact
+      ),
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <TitleForm>Phonebook</TitleForm>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <TitleContacts>Contacts</TitleContacts>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList
+          getContactList={this.getContactList()}
+          onDeleteContact={this.onDeleteContact}
+        ></ContactList>
+      </>
+    );
+  }
+}
+
+export default App;
